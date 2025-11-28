eff3b5d0fceb8b5e993a458c23ba56e1edce1c1c
赵子杭 <16289459+zihangzhao1216@user.noreply.gitee.com>
2025-11-29 06:07:56 +0800

    Defines the Java abstract class Person Unified management of basic attributes (id, name, age), and provision of attribute initialization and access/modification rules. At the same time, because of abstract class characteristics, it cannot be directly instantiated, and needs to be inherited and reused as a parent class by child classes.
    AssignmentTwo
    
    Person
    
    Employee
    
    Visitor
    
    Ride
    
    In AssignmentTwo, the empty method framework has been written.

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,46 @@
-public class Person {
+//Abstract class Person
+public abstract class Person {
+    //Private member variable
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    //Parameterless constructor
+    public Person() {
+        this.id = 0;
+        this.name = "unknown";
+        this.age = 0;
+    }
+
+    //Parametric constructor
+    public Person(Integer pId, String pName, Integer pAge) {
+        this.id = pId;
+        this.name = pName;
+        this.age = pAge;
+    }
+
+    // getter / setter method
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer pId) {
+        this.id = pId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String pName) {
+        this.name = pName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer pAge) {
+        this.age = pAge;
+    }
 }
